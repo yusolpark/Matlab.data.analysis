408ca64abe1b034c8c983474f120d9b176f98f10
Matt Isaacson <misaacson01@gmail.com>
2020-04-23 09:30:59 -0400

    Added notes and files for analyzing data from multiple mice

--- Measurements.m
+++ Measurements.m
@@ -1,40 +1,43 @@
 
 % type mouse and protocal number
-mouse = 1;
+mouse = [1 2]; %will load files from multiple mice
 protocol = 1;
 
-%F = '/Users/yusolpark/Desktop/Matlab.data.analysis';
- F = 'C:\Users\Matthew\Documents\Schaffer-Nishimura Lab\ROArena\Data\dummydata'; %Matt's save location
+% F = '/Users/yusolpark/Desktop/Matlab.data.analysis';
+F = 'C:\Users\Matthew\Documents\Schaffer-Nishimura Lab\ROArena\Data\dummydata'; %Matt's save location
 
+max_size = [10 4 3]; %add a new dimension for number of mice
+multidata = NaN(max_size); 
+
+%%%loop for multiple mice
 S = sprintf('* Mouse%03d Protocol%d *.mat', mouse, protocol);
 files = dir(fullfile(F,S));
 numfiles = length(files);
 
-max_size = [10 4 numfiles]; %This is how big the array should be to hold all the data
-multidata = NaN(max_size); 
-
 for k = 1:numfiles 
   load(fullfile(F,files(k).name));
   [nRow,nCol] = size(param.data); 
-  multidata(1:nRow,1:nCol,k) = param.data; 
+  multidata(1:nRow,1:nCol,k) = param.data; %add a new dimension for mouse
 end
-%size and organization of multidata at this point is [trials=10, datatypes=4, experiments=3]
+%%%end loop for multiple mice
+%size and organization of multidata at this point is [mice=2, trials=10, datatypes=4, experiments=3]
 
-[nRow,nCol,~] = size(multidata); 
 correct = multidata(:,2,:)== multidata(:,3,:); 
-%size of correct is [trials=10, datatype=1, exps=3]
-
+%size of correct is [mice=2, trials=10, datatype=1, exps=3]
 
 avgT = nanmean(multidata(:,4,:)); 
 perCor = nanmean(correct); 
-%size of avgT and perCor is [trialavg=1, datatype=1, exps=3]
+%size of avgT and perCor is [mice=2 trialavg=1, datatype=1, exps=3]
 
-%reshaped into 2-D array
+%%%calculate average of all mice
+%size of ___ and ___ is [miceavg=1 trialavg=1, datatype=1, exps=3]
+
+%reshaped mouse averages into 2-D array
 perCorP = permute(perCor, [1,3,2]); 
 avgTP = permute(avgT, [1,3,2]); 
-%size of perCorP and avgTP is [trialavg=1, exps=3] (since the last dimension=1, it disappears)
+%size of perCorP and avgTP is [miceavg=1, exps=3] (since the last dimensions=1, they disappear)
 
-%plot data 
+%plot data (mouse averages)
 s = 1:numfiles;
 subplot(2,2,1)
 plot(s,perCorP);
